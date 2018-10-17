0b6e438d24643ce28305aa4fc806cea87e820862
BCRXVI <42652884+BCRXVI@users.noreply.github.com>
2018-10-17 12:23:58 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -22,19 +22,19 @@ def render_page3():
 def render_responsemc():
     bom = float(request.args['mk'])
     ans = bom * 1.609
-    return render_template('responsemc.html', response=ans)
+    return render_template('responsemc.html', response=str(bom) + str(ans) + 'km')
 
 @app.route("/responseUSD")
 def render_responseUSD():
     bom = float(request.args['USD'])
     ans = bom * 0.80
-    return render_template('responseUSD.html', response=ans)
+    return render_template('responseUSD.html', response=str(bom) + str(ans) + 'EUR')
 
 @app.route("/responseLBKG")
 def render_responseLBKG():
     bom = float(request.args['Lbs'])
     ans = bom / 2.205
-    return render_template('responseLBKG.html', response=ans)
+    return render_template('responseLBKG.html', response=str(bom) + str(ans) + 'kg')
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
